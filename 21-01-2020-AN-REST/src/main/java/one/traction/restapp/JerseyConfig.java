bbd52e6184b616559f7fee5bdcb51e411fe5c119
Sankarsan Kampa <sankarsan.kampa@accoliteindia.com>
2020-01-29 13:56:38 +0530

    initialize maven project

---
+++ 21-01-2020-AN-REST/src/main/java/one/traction/restapp/JerseyConfig.java
@@ -0,0 +1,10 @@
+package one.traction.restapp;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JerseyConfig extends ResourceConfig {
+    public JerseyConfig() {
+    }
+}
